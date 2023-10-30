55d03cf3838766f14b7220dc11dfdd3916100b17
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-30 05:48:31 -0400

    Update api.py

--- api.py
+++ api.py
@@ -3,3 +3,19 @@ import os,sys,requests,robloxapi
 from flask import Flask, requests, jsonify
 #Define app
 app=Flask(__name__)
+#Defibe views.
+
+# DYNAMIC PAGES
+@app.route('/')
+def index():
+    page="""
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <div></div>
+    </body>
+</html>
+"""
+##end
