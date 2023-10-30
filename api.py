7b26b855bb6b7ec2498b07a42a23e4dcd1adea58
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-30 05:57:05 -0400

    Update api.py

--- api.py
+++ api.py
@@ -9,13 +9,23 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     page="""
+<h1 id='headingText' class='red1 center'>Welcome!</h1>
+<h2 class='red2 center'>Welcome to the EventService Landing Page.</h2>
+<p class='red1 center'>
+    Here you wont exactly find much, but this is The index of the API.<br>
+    API pages can be found under <a href='/api/'>/api/</a>.
+</p>
+"""
+    return """
 <!DOCTYPE html>
 <html>
     <head>
     </head>
     <body>
-        <div></div>
+        <div>
+            """+page+"""
+        </div>
     </body>
 </html>
-"""
+ """
 ##end
