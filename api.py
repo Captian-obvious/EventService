4c8a0f3c3024e8a529f9ab32be0e102e7ce2530f
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-11-09 06:36:29 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 #Modules & Flaak
-import os,sys,requests,robloxapi
+import errors,os,sys,requests,robloxapi
 from flask import Flask, requests, jsonify
 #Define app
 app=Flask(__name__)
@@ -99,5 +99,14 @@ class eventserver:
             ##end
         ##endif
         return ret
+    ##end
+    def error(err,errString):
+        if (err!=None and errString!=None):
+            errMsg={
+                "ErrorCode": errors.getErrorCodeFromString(errString),
+                "ErrorMessage":err,
+                "CompletedStatus":False,
+            }
+            return jsonify(errMsg)
     ##end
 ##end
