63cc1a2217e425fc9985f6d793a748707c1a4303
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-11-09 06:40:11 -0500

    Create errors.py

---
+++ errors.py
@@ -0,0 +1,9 @@
+#Modules&Flask
+import os,requests,robloxapi,sys,time
+from api import app,jsonify,request
+#class errors
+def getErrorCodeFromString(string):
+    if (string=='invalid_input'):
+        
+    ##endif
+##end
