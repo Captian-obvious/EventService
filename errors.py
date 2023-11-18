62f9eeddaad6189edc03923f5d7e5f25932d3ffd
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-11-17 23:26:30 -0500

    Update errors.py

--- errors.py
+++ errors.py
@@ -6,6 +6,20 @@ def getErrorCodeFromString(string):
     errCode=None
     if (string=='prop_invalid'):
         errCode=271
+    elif (string=='access_denied'):
+        errCode=273
+    elif (string=='method_not_allowed'):
+        errCode=274
+    elif (string=='resource_not_found'):
+        errCode=275
+    elif (string=='resource_gone'):
+        errCode=276
+    elif (string=='unauthenticated'):
+        errCode=371
+    elif (string=='invalid_assetid'):
+        errCode=372
+    elif (string=='reserved'):
+        errCode=374
     ##endif
     return errCode
 ##end
